520e5656a3adacce3f45eafe75c2f298805b75a5
Etienne Carriere <etienne.carriere@linaro.org>
2019-11-29 08:17:41 +0100

    drivers: stm32_reset adapt interface to timeout argument
    Changes stm32mp1 reset driver to API to add a timeout argument
    to stm32mp_reset_assert() and stm32mp_reset_deassert() and
    a return value.
    
    With a supplied timeout, the functions wait the target reset state
    is reached before returning. With a null timeout, the functions
    simply load target reset state in SoC interface and return without
    waiting.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- include/drivers/st/stm32mp_reset.h
+++ include/drivers/st/stm32mp_reset.h
@@ -9,7 +9,42 @@
 
 #include <stdint.h>
 
-void stm32mp_reset_assert(uint32_t reset_id);
-void stm32mp_reset_deassert(uint32_t reset_id);
+/*
+ * Assert target reset, if @to_us non null, wait until reset is asserted
+ *
+ * @reset_id: Reset controller ID
+ * @to_us: Timeout in microsecond, or 0 if not waiting
+ * Return 0 on success and -ETIMEDOUT if waiting and timeout expired
+ */
+int stm32mp_reset_assert_to(uint32_t reset_id, unsigned int to_us);
+
+/*
+ * Set reset assert control to reset assertion state and return
+ *
+ * @reset_id: Reset controller ID
+ */
+static inline void stm32mp_reset_assert(uint32_t reset_id)
+{
+	(void)stm32mp_reset_assert_to(reset_id, 0);
+}
+
+/*
+ * Deassert target reset, if @to_us non null, wait until reset is deasserted
+ *
+ * @reset_id: Reset controller ID
+ * @to_us: Timeout in microsecond, or 0 if not waiting
+ * Return 0 on success and -ETIMEDOUT if waiting and timeout expired
+ */
+int stm32mp_reset_deassert_to(uint32_t reset_id, unsigned int to_us);
+
+/*
+ * Set reset assert control to reset deassertion state and return
+ *
+ * @reset_id: Reset controller ID
+ */
+static inline void stm32mp_reset_deassert(uint32_t reset_id)
+{
+	(void)stm32mp_reset_deassert_to(reset_id, 0);
+}
 
 #endif /* STM32MP_RESET_H */
