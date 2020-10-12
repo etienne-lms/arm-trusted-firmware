8b6c06b0a005bf26cb9362bc74ee3bd67db5a6e2
Etienne Carriere <etienne.carriere@linaro.org>
2020-10-12 09:00:08 +0200

    scmi-msg: fix indentation in reset domain
    Minor indentation fixes in scmi-msg reset domain driver.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- drivers/st/scmi-msg/reset_domain.c
+++ drivers/st/scmi-msg/reset_domain.c
@@ -26,21 +26,21 @@ size_t plat_scmi_rstd_count(unsigned int agent_id __unused)
 }
 
 const char *plat_scmi_rstd_get_name(unsigned int agent_id __unused,
-				  unsigned int scmi_id __unused)
+				    unsigned int scmi_id __unused)
 {
 	return NULL;
 }
 
 int32_t plat_scmi_rstd_autonomous(unsigned int agent_id __unused,
-				unsigned int scmi_id __unused,
-				unsigned int state __unused)
+				  unsigned int scmi_id __unused,
+				  unsigned int state __unused)
 {
 	return SCMI_NOT_SUPPORTED;
 }
 
 int32_t plat_scmi_rstd_set_state(unsigned int agent_id __unused,
-			       unsigned int scmi_id __unused,
-			       bool assert_not_deassert __unused)
+				 unsigned int scmi_id __unused,
+				 bool assert_not_deassert __unused)
 {
 	return SCMI_NOT_SUPPORTED;
 }
